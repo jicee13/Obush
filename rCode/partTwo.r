61f1c49e5619b680224361f2e14e34d259428c94
Jayce Miller <jaycem@smu.edu>
2017-10-26 23:37:12 -0500

    asd

--- rCode/partTwo.r
+++ rCode/partTwo.r
@@ -2,11 +2,14 @@
 library(rpart)
 library("rpart.plot")
 library(caret)
+library(FSelector)
 
-dat = read.csv("/Users/jmiller/Projects/dataMining/projectOne/rCode/projectTwoClean/2001-clean.csv", header=TRUE, sep=",", nrows=150000)
+dat = read.csv("/Users/jmiller/Projects/dataMining/projectOne/rCode/projectTwoClean/2013-clean.csv", header=TRUE, sep=",", nrows=1000000)
 
 dat2 <- dat
 
+dat2$SupervisoryStatus <- as.numeric(as.character(dat2$SupervisoryStatus))
+dat2$Station <- as.numeric(as.character(dat2$Station))
 dat2$Education <- as.numeric(as.character(dat2$Education))
 dat2$Age <- as.numeric(substr(dat2$Age, start = 1, stop = 2))
 dat2$LOS <- as.numeric(sub("-.*|\\+|<", "", dat2$LOS))
@@ -18,8 +21,10 @@ agencies <- names(which(table(dat2$AgencyName)>10000))
 
 
 dat2$LOS <- cut(dat2$LOS,
-  breaks = c(0, 1, 3, 5 ,Inf),
-  labels = c("< 1","1-2","3-4","5+"))
+  breaks = c(0, 1, 7, 12 ,Inf),
+  labels = c("< 1","1-7","7-12","12+"))
+
+
 
 
 dat3 <- dat2[dat2$AgencyName %in% agencies, ]
@@ -27,37 +32,52 @@ dat3 <- dat2[dat2$AgencyName %in% agencies, ]
 dat3$AgencyName <- factor(dat3$AgencyName)
 dat3$Agency <- factor(dat3$Agency)
 
-print(dim(dat3))
+# myData <- c('Education','LOS','Pay','Age','SupervisoryStatus')
+myData <- c('Education','Pay','Age','LOS','SupervisoryStatus','Station')
+new <- dat3[myData]
+print(nrow(new))
+print(ncol(new))
+print(summary(new))
+
+
+# weights <- chi.squared(LOS ~ ., data=new)
+# print(weights)
+#
+# o <- order(weights$attr_importance)
+# dotchart(weights$attr_importance[o], labels = rownames(weights)[o],
+#   xlab = "Importance")
+
+# print(dim(dat3))
 
 sample_ID <- sample(1:nrow(dat3), size = 10000)
 dat4 <- dat3[sample_ID, ]
 
-model <- rpart(Pay ~ Age + Education, data = dat4)
+# model <- rpart(Pay ~ Age + Education, data = dat4)
 
 
-
-############## MODEL DONE IN CLASS###################
-# fit <- train(Pay ~ Age + Education + LOS, data = dat4 , method = "rpart",
-#   na.action = na.pass,
-#   trControl = trainControl(method = "cv", number = 10),
-#   tuneLength=10)
 #
-# rpart.plot(fit$finalModel, extra = 2, under = TRUE, varlen=0, faclen=0)
+# ############## MODEL DONE IN CLASS###################
+# # fit <- train(Pay ~ Age + Education + LOS, data = dat4 , method = "rpart",
+# #   na.action = na.pass,
+# #   trControl = trainControl(method = "cv", number = 10),
+# #   tuneLength=10)
+# #
+# # rpart.plot(fit$finalModel, extra = 2, under = TRUE, varlen=0, faclen=0)
+# #
+# # print(varImp(fit))
+# #
+# # testing <- dat3[-sample_ID, ]
+# # testing <- testing[sample(1:nrow(testing), size = 1000), ]
+# #
+# # pred <- predict(fit, newdata = testing, na.action = na.pass)
+# # print(head(pred))
+# #
+# # print(confusionMatrix(data = pred, testing$Pay))
+# ####################################################
 #
-# print(varImp(fit))
 #
-# testing <- dat3[-sample_ID, ]
-# testing <- testing[sample(1:nrow(testing), size = 1000), ]
-#
-# pred <- predict(fit, newdata = testing, na.action = na.pass)
-# print(head(pred))
-#
-# print(confusionMatrix(data = pred, testing$Pay))
-####################################################
-
-
 ############## MODEL OF LOS ###################
-fit <- train(LOS ~ Age + Education, data = dat4 , method = "rpart",
+fit <- train(LOS ~ Age + Pay, data = dat4 , method = "rpart",
   na.action = na.omit,
   trControl = trainControl(method = "cv", number = 10),
   tuneLength=10)
